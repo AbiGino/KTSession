eef437a74c2f19c7ade6a3a4a0399929a8af042b
AbiGino <115475078+AbiGino@users.noreply.github.com>
2023-08-23 12:14:57 +0530

    initial commit

---
+++ src/test/java/com/ktsessiongit/ktsessiongit/KtsessiongitApplication.java
@@ -0,0 +1,16 @@
+package com.ktsessiongit.ktsessiongit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtsessiongitApplication{
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KtsessiongitApplication.class, args);
+		System.out.println("Hello world!");
+		System.out.println("git changes");
+	}
+
+}
